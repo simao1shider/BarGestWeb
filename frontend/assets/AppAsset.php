ec95cb48db432c2ab3cb69295ed996e13b3697b5
Simão Marques <simao.shider@gmail.com>
2020-11-06 22:09:48 +0000

    Definições da app

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,12 +12,13 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        //'css/site.css',
+        'css/site.css',
     ];
     public $js = [
     ];
     public $depends = [
-        //'yii\web\YiiAsset',
+        'yii\web\YiiAsset',
         'yii\bootstrap4\BootstrapAsset',
+        'yii\web\JqueryAsset',
     ];
 }
