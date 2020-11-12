9547777cf9503dade10a82acf3ef916b44f4e96d
Simão Marques <simao.shider@gmail.com>
2020-11-12 22:49:58 +0000

    #PROJ2021 #finish correção de url

--- frontend/views/category/view.php
+++ frontend/views/category/view.php
@@ -49,7 +49,7 @@ $this->title = $category->id;
                 foreach ($category->products as $product){
             ?>
             <div class="col-4 mt-3">
-                <a href="/index.php?r=category%2Fview&id=1">
+                <a href="/product/view/?id=<?= $product->id ?>">
                     <div class="card shadow-sm text-center pt-2 pb-2">
                         <div class="card-body">
                             <h5 class="card-title mt-5 mb-5"><?= $product->name ?></h5>
