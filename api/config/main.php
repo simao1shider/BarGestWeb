46531cf2050f4bbb0b7dc425ecd23fbdf01922a8
Yaroslav Antonenko <ysik345@gmail.com>
2020-11-22 18:12:52 +0000

    #PROJ2021-91 #ready-for-testing

---
+++ api/config/main.php
@@ -0,0 +1,64 @@
+<?php
+
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-api',
+    'basePath' => dirname(__DIR__),    
+    'bootstrap' => ['log'],
+    'modules' => [
+        'v1' => [
+            'basePath' => '@api/modules/v1',
+            'class' => 'api\modules\v1\Module'
+        ]
+    ],
+    'components' => [
+        'request' => [
+            'csrfParam' => '_csrf-backend',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
+            'enableCookieValidation' => true,
+            'cookieValidationKey' => 'bargest',
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => false,
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'enableStrictParsing' => false,
+            'showScriptName' => false,
+            'rules' => [
+                [
+                    'class' => 'yii\rest\UrlRule', 
+                    'controller' => 'v1/country',
+                    'pluralize' => 'false',
+                    'extraPatterns' => [
+                        'GET teste' => 'teste',
+                    ],
+
+                ]
+            ],        
+        ]
+    ],
+    'params' => $params,
+];
+
+
+
