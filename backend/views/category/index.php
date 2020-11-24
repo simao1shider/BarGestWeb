684d1e22cc251b7b6f67943e9e7dbce139cdadad
Yaroslav Antonenko <ysik345@gmail.com>
2020-11-24 20:36:30 +0000

    #PROJ2021-162 #ready-for-testing

--- backend/views/category/index.php
+++ backend/views/category/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\ListView;
 
 /* @var $this yii\web\View */
@@ -24,60 +25,22 @@ $this->title = 'Categorias';
 
     <div class="mt-5 container">
         <div class="row">
-            <div class="col-4 mt-3">
-                <a href="/index.php?r=category%2Fview&id=1">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Gins</h5>
+            <?php
+            foreach ($categories as $category){
+                ?>
+                <div class="col-4 mt-3">
+                    <a href="<?=Url::to(["view","id"=>$category->id])?>">
+                        <div class="card shadow-sm text-center pt-2 pb-2">
+                            <div class="card-body">
+                                <h5 class="card-title mt-5 mb-5"><?=$category->name?></h5>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-4 mt-3">
-                <a href="#">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Vinhos</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-4 mt-3">
-                <a href="#">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Whiskys</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-4 mt-3">
-                <a href="#">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Águas</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-4 mt-3">
-                <a href="#">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Refrigerantes</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-4 mt-3">
-                <a href="#">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Cafetaria</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
+                    </a>
+                </div>
+            <?php
+            }
+            ?>
+
         </div>
 
     </div>
