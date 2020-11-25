306a61a68c52823e20b7f2b21abcc5166c9f272c
Yaroslav Antonenko <ysik345@gmail.com>
2020-11-25 22:58:36 +0000

    #PROJ2021-156 #ready-for-testing

--- backend/views/product/_form.php
+++ backend/views/product/_form.php
@@ -10,3 +10,28 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($product, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($product, 'price')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($product, 'profit_margin')->textInput() ?>
+
+    <?= $form->field($product, 'iva_id')->dropDownList(ArrayHelper::map($ivas, 'id', 'rate'),['prompt'=>'Selecione o iva']) ?>
+
+    <?php
+    if(isset($categories)){
+        echo $form->field($product, 'category_id')->dropDownList(ArrayHelper::map($categories, 'id', 'name'),['prompt'=>'Selecione o categoria']);
+    }
+
+    ?>
+    <div class="form-group">
+        <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
--- backend/views/product/create.php
+++ backend/views/product/create.php
@@ -1,7 +1,5 @@
 <?php
 
-use yii\bootstrap4\ActiveForm;
-use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
@@ -16,31 +14,20 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <div class="product-form">
-
-        <?php $form = ActiveForm::begin(); ?>
-
-        <?= $form->field($product, 'name')->textInput(['maxlength' => true]) ?>
-
-        <?= $form->field($product, 'price')->textInput(['maxlength' => true]) ?>
-
-        <?= $form->field($product, 'profit_margin')->textInput() ?>
-
-        <?= $form->field($product, 'iva_id')->dropDownList(ArrayHelper::map($ivas, 'id', 'rate'),['prompt'=>'Selecione o iva']) ?>
-
-        <?php
-        if(isset($categories)){
-            echo $form->field($product, 'category_id')->dropDownList(ArrayHelper::map($categories, 'id', 'name'),['prompt'=>'Selecione o categoria']);
-        }
-
-        ?>
-        <div class="form-group">
-            <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
-        </div>
-
-        <?php ActiveForm::end(); ?>
-
-    </div>
-
+    <?php
+    if(isset($categories)){
+        $this->render('_form', [
+            'product' => $product,
+            'ivas'=>$ivas,
+            'categories'=>$categories,
+        ]);
+    }
+    else{
+        $this->render('_form', [
+            'product' => $product,
+            'ivas'=>$ivas,
+        ]);
+    }
+     ?>
 
 </div>
