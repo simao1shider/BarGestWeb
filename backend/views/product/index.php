b632df1f104530b9afc58c9274cede65449f1ad1
Yaroslav Antonenko <ysik345@gmail.com>
2020-11-25 21:15:19 +0000

    #PROJ2021-155 #ready-for-testing

--- backend/views/product/index.php
+++ backend/views/product/index.php
@@ -4,8 +4,7 @@ use yii\helpers\Html;
 use yii\widgets\ListView;
 
 /* @var $this yii\web\View */
-/* @var $searchModel app\models\ProductSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $products common\models\Product */
 
 $this->title = 'Produtos';
 ?>
@@ -35,60 +34,19 @@ $this->title = 'Produtos';
 
     <div class="mt-5 container">
         <div class="row">
-            <div class="col-4 mt-3">
-                <a href="/index.php?r=category%2Fview&id=1">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Gordons</h5>
+            <?php foreach ($products as $product){
+                ?>
+                <div class="col-4 mt-3">
+                    <a href="/index.php?r=category%2Fview&id=1">
+                        <div class="card shadow-sm text-center pt-2 pb-2">
+                            <div class="card-body">
+                                <h5 class="card-title mt-5 mb-5"><?=$product->name?></h5>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-4 mt-3">
-                <a href="#">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Tanqueray</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-4 mt-3">
-                <a href="#">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Monkey 47</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-4 mt-3">
-                <a href="#">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Hendrick's</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-4 mt-3">
-                <a href="#">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Beefeater</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
-            <div class="col-4 mt-3">
-                <a href="#">
-                    <div class="card shadow-sm text-center pt-2 pb-2">
-                        <div class="card-body">
-                            <h5 class="card-title mt-5 mb-5">Citadelle</h5>
-                        </div>
-                    </div>
-                </a>
-            </div>
+                    </a>
+                </div>
+            <?php
+            }?>
         </div>
     </div>
 
