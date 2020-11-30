a763c7fc55446dafd2f97e6b2dc82a1e2948ccd3
Yaroslav Antonenko <ysik345@gmail.com>
2020-11-30 17:18:28 +0000

    #PROJ2021-111 #ready-for-testing

--- backend/views/employee/update.php
+++ backend/views/employee/update.php
@@ -1,21 +1,39 @@
 <?php
 
+use yii\bootstrap4\ActiveForm;
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\Employee */
+/* @var $employee common\models\Employee */
 
-$this->title = 'Update Employee: ' . $model->name;
+$this->title = 'Update Employee: ' . $employee->name;
 $this->params['breadcrumbs'][] = ['label' => 'Employees', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $employee->name, 'url' => ['view', 'id' => $employee->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="employee-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <div class="employee-form">
+
+        <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($employee, 'name')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($employee, 'email')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($employee, 'phone')->textInput() ?>
+
+        <?= $form->field($employee, 'birthDate')->textInput() ?>
+
+
+        <div class="form-group">
+            <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+    </div>
 
 </div>
