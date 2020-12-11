1158a2c2f5f2b1b3e3eb5f56af70b3bd36e5c80f
Simão Marques <simao.shider@gmail.com>
2020-12-11 17:39:52 +0000

    #PROJ2021-247 #ready-for-testing finalização

--- frontend/views/table/view.php
+++ frontend/views/table/view.php
@@ -19,44 +19,41 @@ $this->title = $model->number;
             <li class="breadcrumb-item active" aria-current="page"><?= Html::encode($this->title) ?></li>
         </ol>
     </nav>
-    <?php
-    if(!isset($_GET['CR'])){
+    <div class="btn-group btn-group-toggle float-right">
+        <a href="<?= Url::to(['request/create', 'tableId' => $model->id]) ?>" class="btn btn-outline-success btn-lg">Criar Conta</a>
+        <?php
+        if (!isset($_GET['CR'])) {
         ?>
-        <p>
-            <?= Html::a('<i class="fa fa-trash"></i>', ['delete', 'id' => $model->id], [
-                'class' => 'btn btn-outline-danger btn-lg float-right',
+            <?= Html::a('Apagar Mesa', ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-outline-danger btn-lg',
                 'data' => [
                     'confirm' => 'Tem a certeza que pretende apagar?',
                     'method' => 'post',
                 ],
-            ]) ?>
-        </p>
-
-    <?php
-    }
-    ?>
-
+            ]) ?> <?php
+        }
+        ?> 
+    </div>
 
     <div class="mt-5 container">
         <div class="list-group">
-            <a href="<?=Url::to(['request/create','tableId'=>$model->id])?>" class="list-group-item list-group-item-action list-group-item-success text-center mb-3"><?= Html::img('@web/img/Icons/tableBlack.png', ['class' => 'align-top', 'style' => 'width: 35px']) ?><span class="h3 ml-3 mt-2">Criar Conta</span></a>
+
             <?php
-            if(empty($model->accounts)){
-                ?>
+            if (empty($model->accounts)) {
+            ?>
                 <h3 class="text-center">Não exitem contas</h3>
-                    <?php
+                <?php
             }
-            foreach ($model->accounts as $account){
-                if(isset($_GET['CR'])){
+            foreach ($model->accounts as $account) {
+                if (isset($_GET['CR'])) {
                 ?>
-                    <a href="<?=Url::to(['request/create','CR'=>1,'account'=>$account->id])?>" class="list-group-item list-group-item-action list-group-item-warning"><?= Html::img('@web/img/Icons/tableBlack.png', ['class' => 'align-top', 'style' => 'width: 35px']) ?><span class="h3 ml-3 mt-2"><?=$account->name?></span></a>
-                    <?php
-                }
-                else{
+                    <a href="<?= Url::to(['request/create', 'CR' => 1, 'account' => $account->id]) ?>" class="list-group-item list-group-item-action list-group-item-warning"><?= Html::img('@web/img/Icons/tableBlack.png', ['class' => 'align-top', 'style' => 'width: 35px']) ?><span class="h3 ml-3 mt-2"><?= $account->name ?></span></a>
+                <?php
+                } else {
                 ?>
 
-                <a href="<?=Url::to(['account/view','id'=>$account->id])?>" class="list-group-item list-group-item-action list-group-item-warning"><?= Html::img('@web/img/Icons/tableBlack.png', ['class' => 'align-top', 'style' => 'width: 35px']) ?><span class="h3 ml-3 mt-2"><?=$account->name?></span></a>
-                <?php
+                    <a href="<?= Url::to(['account/view', 'id' => $account->id]) ?>" class="list-group-item list-group-item-action list-group-item-warning"><?= Html::img('@web/img/Icons/tableBlack.png', ['class' => 'align-top', 'style' => 'width: 35px']) ?><span class="h3 ml-3 mt-2"><?= $account->name ?></span></a>
+            <?php
                 }
             }
             ?>
