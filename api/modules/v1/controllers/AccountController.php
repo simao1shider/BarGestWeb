f270c6fd1195c5a77ab7736a48497eedb52aa285
Simão Marques <simao.shider@gmail.com>
2020-12-11 16:14:44 +0000

    gitflow-feature-stash: PROJ2021-251-autenticao-por-tokens

--- api/modules/v1/controllers/AccountController.php
+++ api/modules/v1/controllers/AccountController.php
@@ -2,10 +2,12 @@
 
 namespace api\modules\v1\controllers;
 
-use common\models\Product;
 use common\models\ProductsToBePaid;
 use yii\rest\ActiveController;
 use yii\web\Response;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\QueryParamAuth;
 
 class AccountController extends ActiveController
 {
@@ -23,6 +25,22 @@ class AccountController extends ActiveController
                 'application/json' => Response::FORMAT_JSON,
 
             ]
+        ];
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                [
+                    'class' => HttpBasicAuth::className(),
+                    'auth' => function ($username, $password){
+                        $user = \common\models\User::findByUsername($username);
+                        if ($user && $user->validatePassword($password)){
+                            return $user;
+                        }
+                        return null;
+                    }
+                ],
+                QueryParamAuth::className(),
+            ],
         ];
         return $behaviors;
     }
