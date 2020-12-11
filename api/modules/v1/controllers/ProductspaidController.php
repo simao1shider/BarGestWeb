4e7302c6043d6aa741ac312598f22afbb7afb23e
Simão Marques <simao.shider@gmail.com>
2020-12-11 16:24:53 +0000

    #PROJ2021-251 #ready-for-testing

--- api/modules/v1/controllers/ProductspaidController.php
+++ api/modules/v1/controllers/ProductspaidController.php
@@ -4,6 +4,9 @@ namespace api\modules\v1\controllers;
 
 use yii\rest\ActiveController;
 use yii\web\Response;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\QueryParamAuth;
 
 class ProductspaidController extends ActiveController
 {
@@ -21,6 +24,22 @@ class ProductspaidController extends ActiveController
                 'application/json' => Response::FORMAT_JSON,
 
             ]
+        ];
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+            'authMethods' => [
+                [
+                    'class' => HttpBasicAuth::className(),
+                    'auth' => function ($username, $password){
+                        $user = \common\models\User::findByUsername($username);
+                        if ($user && $user->validatePassword($password)){
+                            return $user;
+                        }
+                        return null;
+                    }
+                ],
+                QueryParamAuth::className(),
+            ],
         ];
         return $behaviors;
     }
