fbb9dc24cb2e55a5aeba3f64cb4de01e3ff7eb7a
Simão Marques <simao.shider@gmail.com>
2020-12-20 21:33:12 +0000

    #PROJ2021-253 #done alterações de layout

--- backend/views/sale/view.php
+++ backend/views/sale/view.php
@@ -2,54 +2,52 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use \yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $account common\models\Account */
 
-$this->title = $account->name;
-$this->params['breadcrumbs'][] = ['label' => 'Sales', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
-\yii\web\YiiAsset::register($this);
+$this->title = $account->dateTime;
 ?>
 <div class="bill-view container-fluid ml-5">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::img('@web/img/Icons/Color/getMoney.png', ['class' => 'align-top', 'style' => 'width: 66px']) ?><span class="h3 ml-3 mt-2" id="idMesa"><span class="mt-2"><?= Html::encode($this->title) ?></span></h1>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="../site/index">Home</a></li>
+            <li class="breadcrumb-item"><a href="index">Vendas</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><?= Html::encode($this->title) ?></li>
+        </ol>
+    </nav>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $account->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $account->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-    <?= DetailView::widget([
-        'model' => $account,
-        'attributes' => [
-            'id',
-            'name',
-            'dateTime',
-            'status',
-            'total',
-            'Tables_id',
-            'Employees_id',
-            'Cashiers_id',
-        ],
-    ]) ?>
-
-    <?php
-    foreach ($products as $product){
-        ?>
-        <ul>
-            <li><?=$product->product->name?></li>
-            <li><?=$product->quantity?></li>
-            <li><?=$product->product->price?></li>
-        </ul>
-    <?php
-    }
-    ?>
-
+    <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6">
+            <div class="list-group">
+                <?php
+                if (empty($products)) {
+                    echo '<h4>Sem produtos!</h4>';
+                } else {
+                    foreach ($products as $product) {
+                ?>
+                        <span class="list-group-item list-group-item-action list-group-item-secondary">
+                            <div class="row">
+                                <div class="col-6 h3">
+                                    <span class="h3 mt-2" id="idMesa"><?= $product->product->name ?></span>
+                                </div>
+                                <div class="col-3 h3">
+                                    <span class="mt-2"><?= $product->quantity ?></span>
+                                </div>
+                                <div class="col-3 h3">
+                                    <span class="mt-2"><?= $product->product->price ?> €</span>
+                                </div>
+                            </div>
+                        </span>
+                <?php
+                    }
+                }
+                ?>
+            </div>
+        </div>
+    </div>
 </div>
