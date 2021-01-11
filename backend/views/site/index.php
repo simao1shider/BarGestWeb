8494a10c6f08222b8d999868ea6e22e3cc859fcc
Simão Marques <simao.shider@gmail.com>
2021-01-11 19:09:04 +0000

    remover alerta

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -48,12 +48,12 @@ $this->title = 'My Yii Application';
             </div>
         </div>
 
-        <div class="alert alert-danger alert-dismissible alert-message fade show" role="alert">
+        <!--<div class="alert alert-danger alert-dismissible alert-message fade show" role="alert">
             <strong>Holy guacamole!</strong> You should check in on some of those fields below.
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
-        </div>
+        </div>-->
 
     </div>
 </div>
